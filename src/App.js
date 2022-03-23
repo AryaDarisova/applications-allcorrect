870d49deadeb2a8d05fd724e58b7cb08b9eda7e1
AryaDarisova <arisova.darya@yandex.ru>
2022-03-23 13:40:50 +0400

    Project Bible add filter, client_view etc.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import ProjectBibleTemplateBody from "./managed_quality_on_project/project_bible
 import ProjectBibleTemplateTags from "./managed_quality_on_project/project_bible/template_tags/ProjectBibleTemplateTags";
 import ProjectBibleView from "./managed_quality_on_project/project_bible/view/ProjectBibleView";
 import ProjectBibleClientView from "./managed_quality_on_project/project_bible/client_view/ProjectBibleClientView";
-import Test from "./test/Test";
 import logo from "./img/logo1.png";
 
 const styles = {
@@ -46,7 +45,6 @@ function App() {
             <Route path="/managed_quality_on_project/project_bible/template_tags" element={<ProjectBibleTemplateTags /> } />
             <Route path="/managed_quality_on_project/project_bible/view" element={<ProjectBibleView /> } />
             <Route path="/managed_quality_on_project/project_bible/client_view" element={<ProjectBibleClientView /> } />
-            <Route path="/test" element={<Test /> } />
 
 
 
